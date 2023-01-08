2300eccf11e84b4f8ba4cbe9239277ad6397a174
leviheart <15720605398@163.com>
2023-01-08 23:17:54 +0800

    wcx格式化

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
-const express = require('express')
-const data = require('./resData/news.js')
-const data1 = require('./resData/detail.js')
+const express = require("express");
+const data = require("./resData/news.js");
+const data1 = require("./resData/detail.js");
 
-const app = express()
+const app = express();
 // app.use(express.json()) //  body-parser已经在2019年弃用
 // app.use(express.urlencoded({ extended: false })) //中间件解析post
 // 通过post agent本地测试post接口，get接口可以在浏览器上打印
-const port = 3000
+const port = 3000;
 
-app.post('/api/name/', function (req, res) {
-    res.send(data)
-})
+app.post("/api/name/", function (req, res) {
+  res.send(data);
+});
 
-
-app.get('/api/detailed/', function (req, res) {
-    res.send(data1)
-})
-
+app.get("/api/detailed/", function (req, res) {
+  res.send(data1);
+});
 
 app.listen(port, () => {
-    console.log(`express server start at http://localhost:${port}`)
-
-})
+  console.log(`express server start at http://localhost:${port}`);
+});
--- src/resData/detail.js
+++ src/resData/detail.js
@@ -1,15 +1,15 @@
 module.exports = {
-    name: "LEVI",
-    file: "https://pinia.vuejs.org/logo.svg",
-    start_date: "2023-01-06",
-    end_date: "2023-01-09 17:26:32",
-    food: ["1", "3"],
-    car: "1",
-    class_room: 1,
-    class_room1: 5,
-    class_item: {
-        id: 5,
-        label: "五班",
-    },
-    createDate: ["2023-01-06", "2023-01-09 17:26:32"],
+  name: "LEVI",
+  file: "https://pinia.vuejs.org/logo.svg",
+  start_date: "2023-01-06",
+  end_date: "2023-01-09 17:26:32",
+  food: ["1", "3"],
+  car: "1",
+  class_room: 1,
+  class_room1: 5,
+  class_item: {
+    id: 5,
+    label: "五班",
+  },
+  createDate: ["2023-01-06", "2023-01-09 17:26:32"],
 };
