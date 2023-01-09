0aaa5d06fc8720f6dd9d44be6c240acf3b29c630
莱维 <15720605398@163.com>
2023-01-10 01:32:54 +0800

    wcx20230110classitem

--- src/resData/detail.js
+++ src/resData/detail.js
@@ -7,9 +7,27 @@ module.exports = {
   car: "1",
   class_room: 1,
   class_room1: 5,
-  class_item: {
-    id: 5,
-    label: "vue3",
-  },
+  class_item: [
+    {
+      id: 1,
+      label: "vue3",
+    },
+    {
+      id: 2,
+      label: "vue3",
+    },
+    {
+      id: 3,
+      label: "vue3",
+    },
+    {
+      id: 4,
+      label: "vue3",
+    },
+    {
+      id: 5,
+      label: "vue3",
+    },
+  ],
   createDate: ["2023-01-06", "2023-01-09 17:26:32"],
 };
