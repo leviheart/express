c1fbb145d9fb712e65c9c0c75fccdf9aa0534ba4
leviheart <15720605398@163.com>
2023-01-27 00:44:52 +0800

    保存

--- src/resData/detail.js
+++ src/resData/detail.js
@@ -7,7 +7,6 @@ module.exports = {
   car: "1",
   class_room: 1,
   class_room1: 5,
-<<<<<<< HEAD
   class_item: [{
     id: 5,
     label: "vue3",
@@ -18,29 +17,5 @@ module.exports = {
     id: 7,
     label: "vue3.7",
   }],
-=======
-  class_item: [
-    {
-      id: 1,
-      label: "vue3",
-    },
-    {
-      id: 2,
-      label: "vue3",
-    },
-    {
-      id: 3,
-      label: "vue3",
-    },
-    {
-      id: 4,
-      label: "vue3",
-    },
-    {
-      id: 5,
-      label: "vue3",
-    },
-  ],
->>>>>>> 0aaa5d06fc8720f6dd9d44be6c240acf3b29c630
   createDate: ["2023-01-06", "2023-01-09 17:26:32"],
 };
